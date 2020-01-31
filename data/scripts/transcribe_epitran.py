571e408e5b7db8104e35414068d5d2e68771ac56
soskuthy <mrhowardppa@yahoo.com>
2020-01-31 10:18:25 -0800

    epitran data

--- data/scripts/transcribe_epitran.py
+++ data/scripts/transcribe_epitran.py
@@ -1,13 +1,20 @@
-import epitran, csv, os.path
+import epitran, csv, os.path, codecs
 
 with open('/Users/soskuthy/Documents/Research/current/2017/u-fronting/u-fronting/data/raw_data/language_epitran.csv', 'rU') as csvfile:
 	rdr = csv.reader(csvfile, delimiter=',', quotechar='"')
 	c = 0
 	for row in rdr:
-		if c > 1:
-			break
-		if c != 0:
+		if c >  0:
+			epi = epitran.Epitran(row[3])
 			path = os.path.join('/Users/soskuthy/Documents/Research/current/2017/u-fronting/u-fronting/data/raw_data/corpus_data', row[2])
 			print(path)
+			with codecs.open(path, 'r', encoding='utf8') as text_file:
+				txt = [x.strip().split(" ") for x in text_file.readlines()]
+				for l in txt:
+					l[0] = epi.transliterate(l[0])
+				new_path_l = os.path.splitext(path)
+				new_path = new_path_l[0] + "-ipa" + new_path_l[1]
+				with codecs.open(new_path, 'w', encoding='utf8') as out:
+					out.writelines([' '.join(x) + "\n" for x in txt])
 		c += 1
 		#with open()
---
+++ data/scripts/transcribe_phonemize.py
@@ -0,0 +1,28 @@
+import phonemizer, csv, os.path, codecs, re
+from phonemizer.phonemize import phonemize
+
+with open('/Users/soskuthy/Documents/Research/current/2017/u-fronting/u-fronting/data/raw_data/language_phonemize.csv', newline="") as csvfile:
+	rdr = csv.reader(csvfile, delimiter=',', quotechar='"')
+	c = 0
+	for row in rdr:
+		print(row)
+		if c > 7:
+			language = row[3]
+			backend = row[4]
+			#phonemizer.phonemize.phonemize("hello", language="fr-fr", backend="espeak").strip()
+			path = os.path.join('/Users/soskuthy/Documents/Research/current/2017/u-fronting/u-fronting/data/raw_data/corpus_data', row[2])
+			print(path)
+			with codecs.open(path, 'r', encoding='utf8') as text_file:
+				txt = [x.strip().split(" ") for x in text_file.readlines()]
+				cc = 0
+				txt = [x for x in txt if not re.search('(.)\\1{2,}', x[0]) and not re.search('[!"#$%&()*+,./:;<=>?@\\[\\]\\^\\\\_{|}~]', x[0]) and not x[0]==""]
+				words = [x[0] for x in txt]
+				counts = [x[1] for x in txt]
+				words_phon = [x.strip() for x in phonemizer.phonemize.phonemize(words, language=language, backend=backend)]
+				txt_out = list(zip(words_phon, counts))
+				new_path_l = os.path.splitext(path)
+				new_path = new_path_l[0] + "-ipa" + new_path_l[1]
+				with codecs.open(new_path, 'w', encoding='utf8') as out:
+					out.writelines([' '.join(x) + "\n" for x in txt_out])
+		c += 1
+		#with open()
